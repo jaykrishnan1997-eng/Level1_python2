cee44ec07c6a116ab3cb43e32cbbc8c77059c82b
Jay Krishnan <jkrishna@student.42heilbronn.de>
2026-06-24 12:35:29 +0200

    correct way to add final for try and except in ex4

--- ex4/ft_finally_block.py
+++ ex4/ft_finally_block.py
@@ -7,30 +7,42 @@ class PlantError(Exception):
 
 
 def water_plant(plant_name):
-    if not plant_name[0].isupper():
-        try:
-            raise PlantError(f"'{plant_name}'")
-        except PlantError as e:
-            print(f"Caught PlantError: Invalid plant name to water:{e}")
-            print(".. ending tests and returning to main")
+    if not plant_name == plant_name.capitalize():
+        raise PlantError(f"'{plant_name}'")
+        return (0)
     else:
-        print(f"Watering {plant_name}: [OK]")        
+        print(f"Watering {plant_name}: [OK]")
+        return (1)
+
+
+def test_watering_sysytem():
+
+    print("Testing valid plants...")
+    print("Opening watering system")
+    try:
+        for valid_plant in ("Tomato", "Lettuce", "Carrots"):
+            water_plant(valid_plant)
+    except PlantError as e:
+        print(f"Caught PlantError: Invalid plant name to water: {e}")
+        print(".. ending tests and returning to main")
+        return ()
+    finally:
+        print("Closing watering system\n")
+
+    print("Testing invalid plants...")
+    print("Opening watering system")
+    try:
+        for invalid_plant in ("Tomato", "lettuce"):
+            water_plant(invalid_plant)
+    except PlantError as e1:
+        print(f"Caught PlantError: Invalid plant name to water: {e1}")
+        print(".. ending tests and returning to main")
+        return ()
+    finally:
+        print("Closing watering system\n")
 
 
 if __name__ == "__main__":
     print("=== Garden Watering System ===\n")
-
-    print("Testing valid plants...")
-    print("Opening watering system")
-    water_plant("Tomato")
-    water_plant("Lettuce")
-    water_plant("Carrots")
-    print("Closing watering system\n")
-
-    print("Testing invalid plants...")
-    print("Opening watering system")
-    water_plant("Tomato")
-    water_plant("lettuce")
-    print("Closing watering system\n")
-
+    test_watering_sysytem()
     print("Cleanup always happens, even with errors!")
